781e950caf10de5434922a23b0516ae0bdeca9f6
elenaespana <elenaespana@192.168.1.64>
2021-03-27 09:49:43 +0100

    Añadidas validaciones a review

--- src/Components/Reviews/CreateReviewComponent.jsx
+++ src/Components/Reviews/CreateReviewComponent.jsx
@@ -11,46 +11,83 @@ class CreateReviewComponent extends Component {
 			text:"",
 			score:""
 		} 
-		//this.saveReview = this.saveReview.bind(this);
+		this.saveReview = this.saveReview.bind(this);
 	}
 
-	//validate =()=>{}
+	validate =()=>{
+		let textError = "";
+		let scoreError = "";
+
+		if(this.state.text.length == 0){
+			textError = "Cannot be empty";
+		}
+		if(this.state.score.length==0){
+			scoreError == "Cannot be empty";
+		}
+		this.setState({textError});
+		this.setState({scoreError});
+
+		if(textError || scoreError){
+			return false;
+		}else{
+			return true;
+		}
+
+	}
 
 	cancel(){
 		this.props.history.push('/review-list');
 	}
+
+	changeTextHandler = (event) => {
+		this.setState({text: event.target.value})
+	}
+
+	changeScoreHandler = (event) => {
+		this.setState({score: event.target.value})
+	}
+
 	saveReview = (e) =>{
 		e.preventDefault();
-		//isValid
-		//if valid -> guardar
-		//this.props.history.push('review-list');	
+		const isValid = this.validate();
+		if(isValid){
+			this.props.history.push('review-list');
+		}
 	}
 
 
-render(){
-	<div>
-		<br></br>
-		<br></br>
-		<form>
-			<div className="form-group">
-				<label> Review </label>
+	render(){
+		<div>
+			<br></br>
+			<br></br>
+			<form>
+				<div className="form-group">
+					<label> Review </label>
 				
-			</div>
-			<div className="form-group">
-				<label>Text</label>
-				<textarea placeholder="Text"  name="text" type="text-box" className="form-control" value={this.state.text}/>
+				</div>
+				<div className="form-group">
+					<label>Text</label>
+					<textarea placeholder="Text"  name="text" type="text-box" className="form-control" value={this.state.text}
+					onChange={this.changeTextHandler}/>
+					{this.state.textError ?(<div className="ValidatorMessage">
+						{this.state.textError}
+					</div>): null}
 
-			</div>
-			<div className="form-group">
-			<label> Score </label>
-				<input placeholder="Score" name="score" className="form-control" type="number"></input>
-			</div>
-			<button className="AceptButton" onClick={this.saveReview}>Crear Review</button>
-            <button className="CancelButton" onClick={this.cancel.bind(this)} style={{marginLeft: "10px"}}>Cancelar</button>  
-		</form>
+				</div>
+				<div className="form-group">
+				<label> Score </label>
+					<input placeholder="Score" name="score" className="form-control" type="number" value={this.state.score}
+					onChange={this.changeScoreHandler}></input>
+					{this.state.scoreError ?(<div className="ValidatorMessage">
+						{this.state.scoreError}
+					</div>): null}
+				</div>
+				<button className="AceptButton" onClick={this.saveReview}>Crear Review</button>
+           		<button className="CancelButton" onClick={this.cancel.bind(this)} style={{marginLeft: "10px"}}>Cancelar</button>  
+			</form>
 
-	</div>
-}
+		</div>
+	}
 
 }
 
