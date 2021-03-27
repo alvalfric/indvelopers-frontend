ec3c728733e445448e5c244b611184529dadb182
Juan Pablo <juanp_portero9@hotmail.com>
2021-03-27 13:09:00 +0100

    Change to use UrlProvider

--- src/Services/GameService.js
+++ src/Services/GameService.js
@@ -6,7 +6,7 @@ export const GameService = {
 
     async findAll(){
             //return axios.get( UrlProvider.getGamesUrl + `/findAll`).then(res => res.data)
-            return axios.get( `http://localhost:8080/Game/findAll`).then(res => res.data)
+            return axios.get(UrlProvider.getGamesUrl() + '/findAll').then(res => res.data)
             .catch(error => {return error.response.status})
        
     },
