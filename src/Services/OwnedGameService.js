61b4f6182b8d6a500da8565511587bee4e77fdb4
Ferwalrod <ferwalrod@alum.us.es>
2021-03-31 16:22:30 +0200

    cambios de owned game comiteados aqui
    hacer merge de esta rama a la nueva rama

---
+++ src/Services/OwnedGameService.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { UrlProvider } from '../providers/UrlProvider'
+import { AuthService } from './AuthService';
+
+class OwnedGameService{
+
+    findAllMyOwnedGames(){
+        return AuthService.getToken().then(token=>
+            {
+                return axios.get(UrlProvider.getOwnedGamesUrl()+"/findOwnedGames",{
+                    headers:{
+                        'Authorization':'Bearer '+token,
+                        'Accept': '*/*'
+                    }
+                }).catch(error=>{return error})
+            })
+    }
+    buyGame(gameId){
+        return AuthService.getToken().then(token=>{
+            return axios.post(UrlProvider.getOwnedGamesUrl()+"/buy/"+gameId,{
+                headers:{
+                    'Authorization':'Bearer '+token,
+                    'Accept': '*/*'
+                }
+            })
+        })
+    }
+
+}
+export default new OwnedGameService();
