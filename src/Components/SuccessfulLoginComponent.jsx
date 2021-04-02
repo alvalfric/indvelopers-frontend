6c344689eb6e1b2f49a61aa23a12b9e74fb10b83
fragarroa <46722786+fragarroa@users.noreply.github.com>
2021-04-02 18:34:51 +0200

    Typo

--- src/Components/SuccessfulLoginComponent.jsx
+++ src/Components/SuccessfulLoginComponent.jsx
@@ -7,7 +7,7 @@ class LoginComponent extends Component {
             <React.Fragment>
                 <br />
                 <br />
-                <h1>¡Te has logueado correcmente!</h1>
+                <h1>¡Te has logueado correctamente!</h1>
                 <h2>Pulsa aquí para volver a la <a href="/">Página inicial</a></h2>
             </React.Fragment>
         );
