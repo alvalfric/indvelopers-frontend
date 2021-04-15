c5498e39dd1efa53359217ab2a6ccdc83b8ba8b9
fragarroa <fragarroa@alum.us.es>
2021-04-15 20:43:30 +0200

    Tokens changed and minor issues fixed

--- src/Services/OwnedGameService.js
+++ src/Services/OwnedGameService.js
@@ -2,37 +2,36 @@ import axios from 'axios';
 import { UrlProvider } from '../providers/UrlProvider'
 import { AuthService } from './AuthService';
 
-class OwnedGameService{
+class OwnedGameService {
 
-   async findAllMyOwnedGames(){
-        return AuthService.getToken().then(token=>
-            {
-                return axios.get(UrlProvider.getOwnedGamesUrl()+"/findOwnedGames",{
-                    headers:{
-                        'Authorization':'Bearer '+token,
-                        'Accept': '*/*'
-                    }
-                }).catch(error=>{return error})
-            })
-    }
-   async buyGame(gameId){
-        return AuthService.getToken().then(token=>{
-            return axios.post(UrlProvider.getOwnedGamesUrl()+"/buy/"+gameId,[],{
-                headers:{
-                    'Authorization':'Bearer '+token,
+    async findAllMyOwnedGames() {
+        return AuthService.getToken().then(token => {
+            return axios.get(UrlProvider.getOwnedGamesUrl() + "/findOwnedGames", {
+                headers: {
+                    'Authorization': 'Bearer ' + token,
                     'Accept': '*/*'
                 }
-            })
+            }).catch(error => { return error })
         })
     }
-    async CheckGameOwned(gameId){
-        return AuthService.getToken().then(token=>{
-            return axios.get(UrlProvider.getOwnedGamesUrl()+"/checkGameOwned/"+gameId,{
-                headers:{
-                    'Authorization':'Bearer '+token,
+    async buyGame(gameId) {
+        return AuthService.getToken().then(token => {
+            return axios.post(UrlProvider.getOwnedGamesUrl() + "/buy/" + gameId, [], {
+                headers: {
+                    'Authorization': 'Bearer ' + token,
                     'Accept': '*/*'
                 }
-            })
+            }).catch(error => { return error })
+        })
+    }
+    async CheckGameOwned(gameId) {
+        return AuthService.getToken().then(token => {
+            return axios.get(UrlProvider.getOwnedGamesUrl() + "/checkGameOwned/" + gameId, {
+                headers: {
+                    'Authorization': 'Bearer ' + token,
+                    'Accept': '*/*'
+                }
+            }).catch(error => { return error })
         })
     }
 
