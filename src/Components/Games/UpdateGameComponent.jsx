8803891f3e83ce58a5ae212a1438966594d47eb4
fragarroa <fragarroa@alum.us.es>
2021-04-29 20:27:37 +0200

    Some changes to visual style

--- src/Components/Games/UpdateGameComponent.jsx
+++ src/Components/Games/UpdateGameComponent.jsx
@@ -392,7 +392,7 @@ class UpdateGameComponent extends Component {
                                 <React.Fragment>
                                     
                                     <div className="w3-display-container w3-text-white">
-                                        <img src={"data:image/png;base64," + this.state.base64TextString} style={{ marginLeft: "auto", marginRight: "auto", display: "block" }} width="400" height="300" />
+                                        <img src={"data:image/png;base64," + this.state.base64TextString} style={{ marginLeft: "auto", marginRight: "auto", display: "block", maxWidth: '800px', maxHeight: '400px', }} />
                                         <div className="w3-xlarge w3-display-bottomleft w3-padding" >{this.state.title}</div>
                                     </div>
                                 </React.Fragment>
