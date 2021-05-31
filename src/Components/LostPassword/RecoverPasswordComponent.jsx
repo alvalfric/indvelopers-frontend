d5bd6e8ce3ea9c78acb0fecc038518e66d614ee6
Alvaro <alvalfric@alum.us.es>
2021-05-31 11:35:42 +0200

    Final deployment

--- src/Components/LostPassword/RecoverPasswordComponent.jsx
+++ src/Components/LostPassword/RecoverPasswordComponent.jsx
@@ -66,7 +66,7 @@ class RecoverPasswordComponent extends Component {
                     this.sendFeedback(templateId, {
                         email: this.state.email,
                         to_name: data.username,
-                        passwordReset: "https://continuous-indvelopers.herokuapp.com/restorePassword/" + data.id
+                        passwordReset: "https://indvelopers.herokuapp.com/restorePassword/" + data.id
                     })
                 } else {
                     this.setState({ submitError: "That email is not registered!" });
